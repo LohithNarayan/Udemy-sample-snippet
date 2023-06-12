aabbe529a5fcc3f779f5de36b51b4e070500bb4b
LohithNarayan <a.lohith@ringcentral.com>
2023-06-13 00:42:33 +0530

    Loops examples

---
+++ loops_ex.py
@@ -0,0 +1,31 @@
+x=0
+while x<5:
+    print(f'The current value of x is {x}')
+    x=x+1
+else:
+    print("now x is not less than 5")
+
+    ##############
+    #'continue' key example
+
+    mystring="sammy"
+    for letter in mystring:
+        if letter=="a":
+            continue
+        print(letter )
+        print("the below example for break key")
+
+        ###############
+        #"break" key example
+
+        mystring="lohith"
+        for letter in mystring:
+         if letter=="l":
+            break
+        print(letter)
+
+        #range operator
+        print("Below is the range operator")
+        mylist=[1,2,3]
+        for num in range(0,10,2): #skips 2 digits
+           print(num)
